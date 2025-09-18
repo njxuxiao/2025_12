f59829727144572bee344abbfca20ae7952db86d
njxuxiao <njxuxiao@foxmail.com>
2025-09-19 00:00:38 +0800

    4_21_12 grammar & reading

--- GRAMMAR/grammar.py
+++ GRAMMAR/grammar.py
@@ -417,7 +417,8 @@ def study_helper(file_path, sheet_to_study=None, tts_mode='auto'):
 
 if __name__ == '__main__':
     
-    excel_list = ["1_21_07.xlsx", "2_22_12.xlsx"]
+    excel_list = ["1_21_07.xlsx", "2_22_12.xlsx", 
+                  "3_22_07", "4_21_12"]
 
     excel_file_path = excel_list[0]
     
