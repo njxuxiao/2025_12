35bf081ebd09c17fb46671017ae03053d117d97a
njxuxiao <njxuxiao@foxmail.com>
2025-10-09 08:04:34 +0800

    recite grammar 1-4

--- GRAMMAR/grammar.py
+++ GRAMMAR/grammar.py
@@ -481,7 +481,7 @@ if __name__ == '__main__':
                   "5_20_12.xlsx", "6_19_12.xlsx",
                   "7_19_07.xlsx"]
 
-    excel_file_path = excel_list[6]
+    excel_file_path = excel_list[3]
     
     study_sheet = 0
 
