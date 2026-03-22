f5243bfa96a76a4a676a7d4b6e9979278bcd1629
njxuxiao <njxuxiao@foxmail.com>
2026-03-22 22:47:44 +0800

    recite mixed

--- Mixed/mixed.py
+++ Mixed/mixed.py
@@ -584,7 +584,7 @@ if __name__ == '__main__':
     # Note: Ensure this file exists or change to your filename
     excel_file_path = excel_list[1]
     
-    study_sheet = 0
+    study_sheet = 2
 
     preferred_tts_engine = 'auto' 
 
