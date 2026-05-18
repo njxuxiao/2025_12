a5777d3f9743d1a879c82473c77f74197b84970a
njxuxiao <njxuxiao@foxmail.com>
2026-05-18 08:37:15 +0800

    update listen part4

--- LISTEN/Script/listen.py
+++ LISTEN/Script/listen.py
@@ -413,7 +413,7 @@ def sentence_listening_practice(filepath, repeat_times=3, whisper_model="base"):
         pygame.quit()
 
 if __name__ == '__main__':
-    audio_file = './22_12/1_1.mp3'
+    audio_file = './22_12/1_5.mp3'
     sentence_listening_practice(
         filepath=audio_file, 
         repeat_times=1,  
