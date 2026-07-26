e22137cf651cfb091fdc2393985fd1a1788c7f48
njxuxiao <njxuxiao@foxmail.com>
2026-07-26 23:30:32 +0800

    add grammar 2

--- LISTEN/Script/listen.py
+++ LISTEN/Script/listen.py
@@ -413,7 +413,7 @@ def sentence_listening_practice(filepath, repeat_times=3, whisper_model="base"):
         pygame.quit()
 
 if __name__ == '__main__':
-    audio_file = './1_21_7/3_3.mp3'
+    audio_file = './1_21_7/2_4.mp3'
     sentence_listening_practice(
         filepath=audio_file, 
         repeat_times=1,  
